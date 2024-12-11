b22bf3ed55ab2d4295fb2f23a52aa70d6cae580b
adrims91 <adrims91@gmail.com>
2024-12-11 20:20:23 +0000

    finished

--- src/app.py
+++ src/app.py
@@ -6,39 +6,64 @@ from flask import Flask, request, jsonify, url_for
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from datastructures import FamilyStructure
-#from models import Person
+
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 CORS(app)
 
-# create the jackson family object
+
 jackson_family = FamilyStructure("Jackson")
 
-# Handle/serialize errors like a JSON object
+
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
-# generate sitemap with all your endpoints
+
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
 @app.route('/members', methods=['GET'])
-def handle_hello():
-
-    # this is how you can use the Family datastructure by calling its methods
+def get_all_members():
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
+    return jsonify(members), 200
 
+@app.route('/member', methods=['POST'])
+def add_member():
+    member = request.json
+    added_member = jackson_family.add_member(member)
+    return jsonify(added_member), 200
 
-    return jsonify(response_body), 200
+@app.route('/member/<int:id>', methods=['DELETE'])
+def delete_member(id):
+    result = jackson_family.delete_member(id)
+    return jsonify(result), 200 if result["done"] else 404
 
-# this only runs if `$ python src/app.py` is executed
+@app.route('/member/<int:id>', methods=['PUT'])
+def update_member(id):
+    updated_member = request.json
+    member = jackson_family.update_member(id, updated_member)
+    if member:
+        return jsonify(member), 200
+    else:
+        return jsonify({"msg": "Member not found"}), 404
+
+@app.route('/member/<int:id>', methods=['GET'])
+def get_member(id):
+    member = jackson_family.get_member(id)
+    if member:
+        result = {
+            "first_name": member.get("first_name"),
+            "id": member.get("id"),
+            "age": member.get("age"),
+            "lucky_numbers": member.get("lucky_numbers")
+        }
+        return jsonify(result), 200
+    else:
+        return jsonify({"msg": "Member not found"}), 404
+
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=True)
